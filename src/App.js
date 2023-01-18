e77cbc142cd15907ce2409f3fdfd79bdcacd86a1
Leehaney gee <georgeleehaney20@gmail.com>
2023-01-18 17:23:25 +0300

    Feat: Add working navbar

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import {
 } from 'react-router-dom';
 import Books from './pages/Books';
 import Categories from './pages/Categories';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Books />} />
-      <Route path="/categories" element={<Categories />} />
-    </Routes>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Navigation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
+
+const Navigation = () => (
+  <header className="header">
+    <nav className="nav">
+      <NavLink to="/">Bookstore CMS</NavLink>
+      <ul className="nav">
+        <li className="navlist">
+          <NavLink className="navlink" to="/">BOOKS</NavLink>
+        </li>
+        <li className="navlist">
+          <NavLink className="navlink" to="/categories">CATEGORIES</NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Navigation;
