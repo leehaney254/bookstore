3cc61af5e514179af5490751a34e7b134cd18737
Leehaney gee <georgeleehaney20@gmail.com>
2023-01-18 17:00:57 +0300

    Feat: Create two routes

---
+++ src/pages/Books.js
@@ -0,0 +1,5 @@
+const Books = () => (
+  <h1>Hello! This is the books page</h1>
+);
+
+export default Books;
---
+++ src/pages/Categories.js
@@ -0,0 +1,5 @@
+const Categories = () => (
+  <h1>Hello! Wolcome to the category page.</h1>
+);
+
+export default Categories;
