f9b4f566d6a1e573eec715809a327a1137c95f4b
Leehaney gee <georgeleehaney20@gmail.com>
2023-01-25 11:30:27 +0300

    Feat: Add dummy array to bookstore

--- src/pages/BooksPage.js
+++ src/pages/BooksPage.js
@@ -1,36 +1,11 @@
+import { useSelector } from 'react-redux';
 import Book from '../components/Book';
 import Form from '../components/Form';
 import Navigation from '../components/Navigation';
 
-const books = [
-  {
-    id: 1,
-    title: ' The Catcher in the Rye ',
-    author: ' J.D. Salinger ',
-  },
-  {
-    id: 2,
-    title: ' To Kill a Mockingbird ',
-    author: ' Harper Lee ',
-  },
-  {
-    id: 3,
-    title: ' The Great Gatsby ',
-    author: ' F. Scott Fitzgerald ',
-  },
-  {
-    id: 4,
-    title: ' One Hundred Years of Solitude ',
-    author: ' Gabriel Garcia Marquez ',
-  },
-  {
-    id: 5,
-    title: ' The Lord of the Rings ',
-    author: ' J.R.R. Tolkien ',
-  },
-];
-
 function BooksPage() {
+  const books = useSelector((state) => state.bookState);
+  console.log(books);
   const library = books.map((book) => (
     <Book key={book.id} title={book.title} author={book.author} />
   ));
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,7 +1,36 @@
+import { v4 as uuid } from 'uuid';
+
+const initialState = [
+  {
+    id: uuid(),
+    title: ' The Catcher in the Rye ',
+    author: ' J.D. Salinger ',
+  },
+  {
+    id: uuid(),
+    title: ' To Kill a Mockingbird ',
+    author: ' Harper Lee ',
+  },
+  {
+    id: uuid(),
+    title: ' The Great Gatsby ',
+    author: ' F. Scott Fitzgerald ',
+  },
+  {
+    id: uuid(),
+    title: ' One Hundred Years of Solitude ',
+    author: ' Gabriel Garcia Marquez ',
+  },
+  {
+    id: uuid(),
+    title: ' The Lord of the Rings ',
+    author: ' J.R.R. Tolkien ',
+  },
+];
+
 // Define constants
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
-const initialState = [];
 
 // Actions for adding and removing books
 const addBook = (book) => (
@@ -19,17 +48,17 @@ const removeBook = (id) => (
 );
 
 // Create reducer
-const BooksReducer = (state = initialState, action) => {
+const BooksReducer = (bookState = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [
-        ...state,
+        ...bookState,
         action.payload,
       ];
     case REMOVE_BOOK:
-      return [...state.filter((book) => book !== action.payload)];
+      return [...bookState.filter((book) => book !== action.payload)];
     default:
-      return state;
+      return bookState;
   }
 };
 
