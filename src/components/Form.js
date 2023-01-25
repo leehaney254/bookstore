b5c0b15e23ed4733bcf46641ee4cc6318645c395
Leehaney gee <georgeleehaney20@gmail.com>
2023-01-25 12:24:36 +0300

    Feat: Validate form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,46 @@
+import { useState } from 'react';
 import './Form.css';
 
-const Form = () => (
-  <>
-    <h2 className="formlabel">Add New book</h2>
-    <form>
-      <label htmlFor="title" className="formlabel">
-        <input type="text" id="title" name="title" placeholder="Title" />
-      </label>
-      <label htmlFor="author" className="formlabel">
-        <input type="text" id="author" name="author" placeholder="Author" />
-      </label>
-      <button type="submit">Add Book</button>
-    </form>
-  </>
-);
+const Form = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [formIsNotValid, setformIsNotValid] = useState(false);
+
+  const titleChangeHandler = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const authorChangeHandler = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (title.trim() === '' || author.trim() === '') {
+      setformIsNotValid(true);
+      return;
+    }
+    setformIsNotValid(false);
+    console.log(title);
+    console.log(author);
+  };
+
+  return (
+    <>
+      <h2 className="formlabel">Add New book</h2>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="title" className="formlabel">
+          <input type="text" id="title" value={title} onChange={titleChangeHandler} name="title" placeholder="Title" />
+        </label>
+        <label htmlFor="author" className="formlabel">
+          <input type="text" id="author" value={author} onChange={authorChangeHandler} name="author" placeholder="Author" />
+        </label>
+        <button type="submit">Add Book</button>
+        {formIsNotValid && <div id="warning">Do not leave any empty fields</div>}
+      </form>
+    </>
+  );
+};
 
 export default Form;
