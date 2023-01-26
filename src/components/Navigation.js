29f458dcf33c9352842cc259bf598fc0556a8773
Leehaney gee <georgeleehaney20@gmail.com>
2023-01-26 19:20:45 +0300

    Style: add active link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,41 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 import './Navbar.css';
 import profile from '../assets/profile.png';
 
-const Navigation = () => (
-  <header>
-    <nav className="nav-bar">
-      <NavLink id="logo" to="/">Bookstore CMS</NavLink>
-      <ul className="nav-links">
-        <li className="navlist">
-          <NavLink className="navlink" to="/">BOOKS</NavLink>
-        </li>
-        <li className="navlist">
-          <NavLink className="navlink" to="/categories">CATEGORIES</NavLink>
-        </li>
-      </ul>
-      <button type="button" className="icon-button"><img src={profile} alt="profile" /></button>
-    </nav>
-  </header>
-);
+const Navigation = () => {
+  const [BookActiveLink, setBookActiveLink] = useState(false);
+  const [categoryActiveLink, setCategoryActiveLink] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    const path = location.pathname;
+    if (path === '/') {
+      setBookActiveLink(true);
+      setCategoryActiveLink(false);
+    }
+    if (path === '/categories') {
+      setBookActiveLink(false);
+      setCategoryActiveLink(true);
+    }
+  }, [location]);
+
+  return (
+    <header>
+      <nav className="nav-bar">
+        <NavLink id="logo" to="/">Bookstore CMS</NavLink>
+        <ul className="nav-links">
+          <li className="navlist">
+            <NavLink className={BookActiveLink ? 'activelink navlink' : 'navlink'} to="/">BOOKS</NavLink>
+          </li>
+          <li className="navlist">
+            <NavLink className={categoryActiveLink ? 'activelink navlink' : 'navlink'} to="/categories">CATEGORIES</NavLink>
+          </li>
+        </ul>
+        <button type="button" className="icon-button"><img src={profile} alt="profile" /></button>
+      </nav>
+    </header>
+  );
+};
 
 export default Navigation;
